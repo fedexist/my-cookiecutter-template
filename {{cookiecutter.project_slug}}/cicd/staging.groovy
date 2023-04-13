7bc60a96287763c41b26dff36970738f7da0ca33
Federico D'Ambrosio <federico.dambrosio@generali.com>
2023-04-13 09:22:30 +0200

    Update template

--- {{cookiecutter.project_slug}}/cicd/staging.groovy
+++ {{cookiecutter.project_slug}}/cicd/staging.groovy
@@ -1,15 +1,15 @@
 // Auto-Generated Variables
-env.ENV = "prd"
+env.ENV = "kns"
 
-env.STATE_BUCKET = "generali-it-prod-mlt-advana-000001-tfstate"
+env.STATE_BUCKET = "generali-it-nonprod-mlt-advana-000013-tfstate"
 env.TERRAFORM_SA_CRED_NAME = "terraform-${env.LE}-${env.ENV}-json"
 
 env.PYPI_REGISTRY = "europe-west3-python.pkg.dev"
 env.DOCKER_REGISTRY = "europe-west3-docker.pkg.dev"
-env.ARTIFACT_PROJECT = "it-prod-gen-advana-000040"
+env.ARTIFACT_PROJECT = "it-nonprod-gen-advana-000008"
 env.PYPI_REPO = "https://${PYPI_REGISTRY}/it-prod-gen-advana-000040/pypi/"
 env.DOCKER_URL = "${DOCKER_REGISTRY}/${ARTIFACT_PROJECT}/docker-images"
 env.BASE_IMAGE = "${DOCKER_URL}/aa-python:3.9-slim"
 
 // User-Defined Variables
-env.TARGET_PROJECT_ID = "it-prod-gen-advana-000040"
+env.TARGET_PROJECT_ID = "it-nonprod-gen-advana-000008"
